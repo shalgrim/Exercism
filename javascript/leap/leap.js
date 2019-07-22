96f9b2f456c30939b564f407499cbe3d5e9259a6
Scott Halgrim <shalgrim@gmail.com>
2019-07-22 16:57:13 -0700

    feat: leap
    This is the solution to the leap program

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,8 +1,10 @@
-//
-// This is only a SKELETON file for the 'Leap' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-export const isLeap = () => {
-  throw new Error("Remove this statement and implement this function");
+export const isLeap = (year) => {
+  if (year % 4 > 0) {
+    return false;
+  } else if (year % 400 == 0) {
+    return true;
+  } else if (year % 100 == 0) {
+    return false;
+  }
+  return true;
 };
