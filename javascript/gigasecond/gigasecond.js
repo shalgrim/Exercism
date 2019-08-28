1868f4d0b811daa924f810715d0f62680d4e3f8a
Scott Halgrim <shalgrim@gmail.com>
2019-08-28 14:59:39 -0700

    wth does the mentor want

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -1,6 +1,10 @@
 export const gigasecond = (d) => {
+
+  // a trillion milliseconds is a gigasecond
   const trillion_milliseconds = 1000000000000;
 
+  // these tests pass
+  // but mentor just keeps asking cryptic questions
   const input_time = d.getTime();
   const later_date = new Date();
   later_date.setTime(input_time + trillion_milliseconds);
