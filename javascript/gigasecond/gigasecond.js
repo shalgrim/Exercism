d152709b8b29ddeae28fab6dbab0e0aefc9ff8fb
Scott Halgrim <shalgrim@gmail.com>
2019-08-27 21:10:52 -0700

    got feedback on abbreviation

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -1,4 +1,11 @@
 export const gigasecond = (d) => {
-  d.setSeconds(d.getSeconds() + 1000000000);
+  const billion = 1000000000;
+
+  if (d.getYear() < 60) {
+    d.setSeconds(d.getSeconds() + billion);
+    d.setHours(d.getHours() - 1);
+  } else {
+      d.setSeconds(d.getSeconds() + 1000000000);
+  }
   return d;
 };
