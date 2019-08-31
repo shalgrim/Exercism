db3a351c18e39d8819cf4e0df2e97947b747c52f
Scott Halgrim <shalgrim@gmail.com>
2019-08-31 15:01:17 -0700

    my latest submission my mentor is not being very helpful

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -1,12 +1,4 @@
 export const gigasecond = (d) => {
-
-  // a trillion milliseconds is a gigasecond
-  const trillion_milliseconds = 1000000000000;
-
-  // these tests pass
-  // but mentor just keeps asking cryptic questions
-  const input_time = d.getTime();
-  const later_date = new Date();
-  later_date.setTime(input_time + trillion_milliseconds);
-  return later_date;
-};
+  const A_GIGASECOND = 1e12;
+  return new Date(d.getTime() + A_GIGASECOND);
+}; 
