b4e49e5b979b53e2d4d4ff0c9071f351beec5d79
Scott Halgrim <shalgrim@gmail.com>
2019-09-03 11:41:55 -0700

    rna-transcription: tests pass

---
+++ javascript/rna-transcription/rna-transcription.js
@@ -0,0 +1,16 @@
+const dnaToRnaMap = {
+  'G': 'C',
+  'C': 'G',
+  'T': 'A',
+  'A': 'U'
+};
+
+const dnaToRnaMapper = (d) => {
+  return dnaToRnaMap[d];
+}
+
+export const toRna = (dna) => {
+  const chars = dna.split('');
+  const rnaValues = chars.map(dnaToRnaMapper);
+  return rnaValues.join('');
+};
