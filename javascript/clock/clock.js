471611e6c0cbb8dc1109e8f92d20b16d912e9494
Scott Halgrim <shalgrim@gmail.com>
2019-09-05 13:43:00 -0700

    clock tests pass, though it's far from great

--- javascript/clock/clock.js
+++ javascript/clock/clock.js
@@ -35,14 +35,29 @@ export class Clock {
   }
 
   plus(minute) {
-    throw new Error('Remove this statement and implement this function');
+    const total_minutes = minute + this.minute;
+    this.minute = total_minutes % 60;
+    this.hour = (this.hour + Math.trunc(total_minutes/60)) % 24;
+    return this;
   }
 
-  minus() {
-    throw new Error('Remove this statement and implement this function');
+  minus(minute) {
+    const total_minutes = this.minute - minute;
+
+    if (total_minutes < 0) {
+      this.minute = (total_minutes % 60) + 60;
+      this.hour = (this.hour + Math.trunc(total_minutes/60) - 1);
+      if (this.hour < 0) {
+        this.hour = ((this.hour % 24) + 24) % 24;
+      }
+    } else {
+      this.minute = total_minutes;
+    }
+
+    return this;
   }
 
-  equals() {
-    throw new Error('Remove this statement and implement this function');
+  equals(other) {
+    return this.hour == other.hour && this.minute == other.minute;
   }
 }
