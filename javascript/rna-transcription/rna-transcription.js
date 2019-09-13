358137a723b19ba336377f9a0e1484dac51fedee
Scott Halgrim <shalgrim@gmail.com>
2019-09-13 14:08:04 -0500

    rna transcription: lambda-y thing function
    Move my function into some lambda-y kind of thing

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -5,12 +5,8 @@ const dnaToRnaMap = {
   A: 'U'
 };
 
-const dnaToRnaMapper = (d) => {
-  return dnaToRnaMap[d];
-}
-
 export const toRna = (dna) => {
   const chars = dna.split('');
-  const rnaValues = chars.map(dnaToRnaMapper);
+  const rnaValues = chars.map((d) => ( dnaToRnaMap[d] ));
   return rnaValues.join('');
 };
