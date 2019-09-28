dd4172720af5214f519f6414c500ab9110658680
Scott Halgrim <shalgrim@gmail.com>
2019-09-28 12:29:42 -0700

    strings are not Arrays…

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -3,6 +3,13 @@
 // convenience to get you started writing code faster.
 //
 
-export const compute = () => {
-  throw new Error("Remove this statement and implement this function");
+export const compute = (strand1, strand2) => {
+  const hamming_distance = 0;
+  if (strand1.length != strand2.length) {
+      throw new Error("Can only compare strands of equal length.");
+  }
+
+  strand1.map((character, index) => { character == strand2[index] ? hamming_distance++ : 0});
+
+  return hamming_distance;
 };
