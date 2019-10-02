0547450a93026e415518469cd4314839253d7c7a
Scott Halgrim <shalgrim@gmail.com>
2019-10-02 15:06:56 -0700

    javscript: scrabble-score passes tests

---
+++ javascript/scrabble-score/scrabble-score.js
@@ -0,0 +1,40 @@
+//
+// This is only a SKELETON file for the 'Scrabble Score' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+const lettersToPoints = {
+  A: 1,
+  B: 3,
+  C: 3,
+  D: 2,
+  E: 1,
+  F: 4,
+  G: 2,
+  H: 4,
+  I: 1,
+  J: 8,
+  K: 5,
+  L: 1,
+  M: 3,
+  N: 1,
+  O: 1,
+  P: 3,
+  Q: 10,
+  R: 1,
+  S: 1,
+  T: 1,
+  U: 1,
+  V: 4,
+  W: 4,
+  X: 8,
+  Y: 4,
+  Z: 10
+};
+
+export const score = word => {
+  return word
+    .split("")
+    .map(char => lettersToPoints[char.toUpperCase()])
+    .reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+};
