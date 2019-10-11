a46a8e981ce71b248f76e4495b4928f211d1f5d0
Scott Halgrim <shalgrim@gmail.com>
2019-10-10 19:40:04 -0700

    all tests pass

--- javascript/nucleotide-count/nucleotide-count.js
+++ javascript/nucleotide-count/nucleotide-count.js
@@ -1,19 +1,19 @@
-const NUCLEOTIDES = ['A', 'C', 'G', 'T'];
+const NUCLEOTIDES = ["A", "C", "G", "T"];
 
 export class NucleotideCounts {
-
   static parseNucleotide(nucleotide, dna) {
-    console.log(dna);
-    console.log(typeof(dna));
-    console.log(nucleotide);
-    return (dna.match(new RegExp(nucleotide, 'g')) || []).length
+    return (dna.match(new RegExp(nucleotide, "g")) || []).length;
   }
   static parse(dna) {
     // count A, C, G, and T in dna
-    const counts = NUCLEOTIDES.map(nucleotide => NucleotideCounts.parseNucleotide(nucleotide, dna));
-    const answer = counts.map(count => count.toString()).join(' ');
-    return answer;
+    const counts = NUCLEOTIDES.map(nucleotide =>
+      NucleotideCounts.parseNucleotide(nucleotide, dna)
+    );
+    const answer = counts.map(count => count.toString()).join(" ");
 
-    throw new Error("Remove this statement and implement this function");
+    if (counts.reduce((x, y) => x + y) != dna.length) {
+      throw new Error("Invalid nucleotide in strand");
+    }
+    return answer;
   }
 }
