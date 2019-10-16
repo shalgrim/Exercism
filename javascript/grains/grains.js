fa69f9591681adec22886edc88d5e64ec17c2850
Scott Halgrim <shalgrim@gmail.com>
2019-10-16 11:33:44 -0700

    javascript: grains: first five tests pass

---
+++ javascript/grains/grains.js
@@ -0,0 +1,9 @@
+export class Grains {
+  square(square_number) {
+    return Math.pow(2, square_number-1).toString();
+  }
+
+  total() {
+    throw new Error("Remove this statement and implement this function");
+  }
+}
