87879a829b754b9e5b16032f75c68595b8568ad1
Scott Halgrim <shalgrim@gmail.com>
2019-10-16 11:27:10 -0700

    cleanup

--- javascript/prime-factors/prime-factors.js
+++ javascript/prime-factors/prime-factors.js
@@ -1,5 +1,4 @@
 const isPrime = function(number) {
-  console.log(`isPrime number: ${number}`)
   if (number < 2) {
     return false;
   }
@@ -22,28 +21,16 @@ const isPrime = function(number) {
 }
 
 const primeFactors = number => {
-  console.log(`number: ${number}`)
   let remaining = number;
   const answer = [];
   let lastPrime = 2;
   
-  console.log(`remaining: ${remaining}`)
   while (remaining >=2) {
-    console.log(`remaining: ${remaining}`)
-    // return;  // last place where I can get stuff to stop
     if (isPrime(remaining)) {
-      console.log('entering if')
       answer.push(remaining);
-      console.log(`answer: ${answer}`)
       remaining = 1;
-      console.log(`remaining: ${remaining}`)
     }
-    console.log('through if')
-    // return;  // get it to stop here but not when testing...
 
-    console.log(`lastPrime: ${lastPrime}`); // 2
-    console.log(`remaining: ${remaining}`); // 1...so remaining % i is never 0
-    // return;
     for (let i = lastPrime; remaining > 1; i++) {
       if (isPrime(i) && remaining % i == 0) {
         answer.push(i);
@@ -57,5 +44,4 @@ const primeFactors = number => {
   return answer;
 };
 
-// export {isPrime, primeFactors};
 module.exports = { isPrime, primeFactors };
